8cab0f4be912510d7eebd54a2b8669b5608c8bd7
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-08-06 15:07:07 +0300

    Add project files.

---
+++ Week4Practice2/Baby.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Week4Practice2
+{
+    public class Baby
+    {
+        private string _name;
+        private string _lastName;
+        private DateTime _dateTimeBirth;
+        
+        public Baby()                                   //Parametre almayan bir ctor
+        {
+            Inga();
+            Console.WriteLine(_dateTimeBirth = DateTime.Now); 
+        }
+
+        public Baby(string name, string lastName)                       //Parametreli ctor
+        {
+            _name = name;
+            _lastName = lastName;
+            _dateTimeBirth = DateTime.Now;                              //datetimebirth o anki tarih değerini alır
+            Inga();
+
+        }
+
+
+        public string Name 
+        {
+            get {  return _name; }
+            set {  _name = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime DateTimeBirth
+        {
+            get { return _dateTimeBirth; }
+            set { _dateTimeBirth = value; }
+        }
+
+        public void InfoBaby()                  //bilgileri yazdırmak için oluşturulan metot
+        {
+            Console.WriteLine("İsim : " +_name + " Soyisim : "  +_lastName + " " + " Doğum tarihi : " +_dateTimeBirth);
+        }
+
+        public void Inga()                      //Ağlama metodu
+        {
+            Console.WriteLine("Ingaaa");
+        }
+    }
+}
---
+++ Week4Practice2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Week4Practice2;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-----Parametresiz CTOR------");
+
+        Baby baby1 = new Baby();                                    //Parametresin ctor da nesnenin bilgileri tanımlandı ve bilgilerin yazılması için gerekli motot çağırıldı.
+        baby1.Name = "Ali";
+        baby1.LastName = "Çakıroğlu";
+        baby1.InfoBaby();
+       
+
+
+        Console.WriteLine("-----Parametreli CTOR-------");
+
+        Baby baby2 = new Baby("merve","akkoyunlu");                 //Parametreli ctor olduğu için parametreler tanımlandı ve bilgilerin yazılması için gerekli motot çağırıldı.
+        baby2.InfoBaby();
+    }
+}
